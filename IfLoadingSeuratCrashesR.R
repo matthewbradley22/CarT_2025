3c269582d727eb31d770371a9aee232cc5625996
Matthew Bradley <79165475+matthewbradley22@users.noreply.github.com>
2025-07-23 11:49:31 +0200

    Update IfLoadingSeuratCrashesR.R

--- IfLoadingSeuratCrashesR.R
+++ IfLoadingSeuratCrashesR.R
@@ -1,4 +1,4 @@
-#Can do this is seurat crashes R
+#Sometimes HPC2N RStudio crashes when Seurat is loaded - can do this to fix
 db <- available.packages() 
 deps <- tools::package_dependencies("Seurat", db)$Seurat 
 install.packages(deps[21:45]) 
